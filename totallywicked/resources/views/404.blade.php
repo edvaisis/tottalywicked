26bbc6bdc608e4e9270418255e57f923c56a1133
edvaisis <edvaisis@gmail.com>
2023-05-22 23:17:37 +0100

    Revert "initial setup"
    This reverts commit 32fce44cda09bbbb54e5b6b0f817161a860b895e.

--- totallywicked/resources/views/404.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layout.app')
-
-<title>404 - Page Not Found</title>
-
-@section('content')
-    <h1>404 - Page Not Found</h1>
-    <p>Page could not be found.</p>
-    <a href="{{ route('home') }}">Go back </a>
-@endsection
